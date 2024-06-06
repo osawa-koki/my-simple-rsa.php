57e51df0b6578765c4804c58ae51b61f942dbed2
osawa-koki <koki@osawa.dev>
2024-06-06 16:32:51 +0900

    メイン実行スクリプトの作成。

--- src/main.php
+++ src/main.php
@@ -1,3 +1,43 @@
 <?php
 
-echo "Hello, World!\n";
+require_once './src/getPrivateKey.php';
+require_once './src/getPublicKey.php';
+require_once './src/encrypt.php';
+require_once './src/decrypt.php';
+
+foreach ($GLOBALS['argv'] as $arg) {
+    if (preg_match('/^p1=(.*)$/', $arg, $matches)) {
+        $GLOBALS['params']['p1'] = $matches[1];
+    }
+    if (preg_match('/^p2=(.*)$/', $arg, $matches)) {
+        $GLOBALS['params']['p2'] = $matches[1];
+    }
+    if (preg_match('/^m=(.*)$/', $arg, $matches)) {
+        $GLOBALS['params']['m'] = $matches[1];
+    }
+}
+
+[$p1, $p2, $m] = [$GLOBALS['params']['p1'], $GLOBALS['params']['p2'], $GLOBALS['params']['m']];
+echo "p1: $p1, p2: $p2, m: $m\n";
+
+if ($p1 == null || $p2 == null || $m == null) {
+    echo "Usage: php main.php p1=XXX p2=XXX m=XXX\n";
+    exit(1);
+}
+
+$publicKey = getPublicKey($p1, $p2);
+$privateKey = getPrivateKey($p1, $p2, $publicKey);
+
+echo "public key: ({$publicKey[0]}, {$publicKey[1]})\n";
+echo "private key: ({$privateKey[0]}, {$privateKey[1]})\n";
+
+$encrypted = encrypt($publicKey, $m);
+$decrypted = decrypt($privateKey, $encrypted);
+
+if ($m != $decrypted) {
+    echo "Error: $m != $decrypted\n";
+    exit(1);
+}
+
+echo "encrypted: $encrypted\n";
+echo "decrypted: $decrypted\n";
